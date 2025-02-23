499e090e1ff8575b16ff1015b6caaab919eedb10
bob <bob@arklarb.com>
2025-02-23 14:32:41 +0800

    init commit

---
+++ lib/socket_manager.dart
@@ -0,0 +1,7 @@
+library socket_manager;
+
+export 'package:socket_manager/ws/core/socket_io.dart';
+export 'package:socket_manager/ws/core/channel_symbol_util.dart';
+export 'package:socket_manager/ws/core/model/ws_receive_data.dart';
+export 'package:socket_manager/ws/core/model/ws_send_data.dart';
+export 'package:socket_manager/ws/core/socket_manager.dart';
